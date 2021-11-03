52d02d2eafc54931c5822d13c6b246393cc9a79e
yamensaad <yamen.saa199@gmail.com>
2021-11-03 11:52:39 +0100

    feat : create car model

--- cars/models.py
+++ cars/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-
+##add cars model
 class Cars( models.Model ):
     Brand = models.TextField( "Brand", max_length=500 )
     Model = models.CharField( "Model", max_length=500 )
